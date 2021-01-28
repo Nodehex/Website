b0c5dce68b756ea893f3d28770992dd3f1897bff
YannikH <yannikhegge@gmail.com>
2021-01-28 23:43:50 +0100

    big overhaul

--- js/index.js
+++ js/index.js
@@ -1,27 +1,19 @@
 window.addEventListener('scroll', e => {
-  const scrollYCap = 200
-  const scrollYCapped = Math.min(window.scrollY, scrollYCap)
-  const scrollYPart = scrollYCapped / scrollYCap
-  resizeHeader(scrollYPart)
+  resizeHeader(window.scrollY / window.innerHeight)
 });
 
 const resizeHeader = (scrollYPart) => {
-  scrollYPart = 1;
-  const barHeight = 150 - 75 * scrollYPart
-  const logoIconTop = 35 - 22.5 * scrollYPart
-  const logoMarginTop = 50 - 37.5 * scrollYPart
-  const iconHeight = 80 - scrollYPart * 30
-  document.querySelector(".brand-logo img").style["margin-top"] = `${logoMarginTop}px`
-  document.querySelector(".icon-fixed-small img").style.height = `${iconHeight}px`
-  document.querySelector(".icon-fixed-small").style.top = `${logoIconTop}px`
-  document.querySelectorAll('nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i')
-    .forEach(element => {
-      element.style.height = `${barHeight}px`
-      element.style["line-height"] = `${barHeight}px`
-    });
-  document.querySelectorAll('nav svg')
-    .forEach(element => element.setAttribute("height", `${barHeight}px`));
+  console.log(scrollYPart)
+  if (scrollYPart > 1) {
+    document.querySelectorAll('nav ul a, nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i')
+      .forEach(element => {
+        console.log(element)
+        element.classList.add('scrolled')
+      });
+  } else {
+    document.querySelectorAll('nav ul a, nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i')
+      .forEach(element => {
+        element.classList.remove('scrolled')
+      });
+  }
 }
-
-
-resizeHeader(0);
