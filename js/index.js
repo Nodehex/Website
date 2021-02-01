5dc2e32935ddee5cd088df5e95ceba221991de6f
spacebjorn <30187002+Spacebjorn@users.noreply.github.com>
2021-02-01 16:33:12 +0100

    removed unused scroll function

--- js/index.js
+++ js/index.js
@@ -5,19 +5,3 @@ window.addEventListener('scroll', e => {
 var el = document.getElementById('service-tabs')
 var instance = M.Tabs.init(el)
 
-const resizeHeader = (scrollYPart) => {
-  console.log(scrollYPart)
-  return;
-  if (scrollYPart > 0.9) {
-    document.querySelectorAll('nav ul a, nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i')
-      .forEach(element => {
-        console.log(element)
-        element.classList.add('scrolled')
-      });
-  } else {
-    document.querySelectorAll('nav ul a, nav, nav .nav-wrapper i, nav a.sidenav-trigger, nav a.sidenav-trigger i')
-      .forEach(element => {
-        element.classList.remove('scrolled')
-      });
-  }
-}
