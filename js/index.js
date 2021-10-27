83f2bb800c8a659fc9e9d754c120afa8ea90483e
Sebastian Olsen <30187002+Spacebjorn@users.noreply.github.com>
2021-10-27 19:56:55 +0200

    added animation to the navbar and made the navbar smaller

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
-let element = document.getElementById('service-tabs')
+let element = document.getElementById("service-tabs")
 let instance = M.Tabs.init(element)
+
+const bigLogo = document.querySelector(".brand-logo")
+const smallLogo = document.querySelector(".small-brand-logo")
+const navBar = document.getElementsByTagName("nav")[0]
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      bigLogo.classList.remove("fade-out")
+      smallLogo.classList.add("fade-out")
+      navBar.classList.remove("tan")
+      return
+    }
+
+    smallLogo.classList.remove("fade-out")
+    bigLogo.classList.add("fade-out")
+    navBar.classList.add("tan")
+  })
+})
+
+observer.observe(document.querySelector(".wrap"))
